748658cacb85703fc161c4e997d9c04bdb1585c8
tia-kazita <kazialuce@gmail.com>
2016-10-26 18:55:57 -0700

    my code before I looked at WCC code

---
+++ Python/guess_again_kl_try.py
@@ -0,0 +1,72 @@
+import random
+
+
+# Get the user's guess
+# Params: None
+# Returns: Integer
+#
+def get_guess():
+
+    # Get initial guess
+    guess = raw_input('Enter your guess: ')
+
+    # Assume it's not valid, until it's proven otherwise
+    valid = False
+
+    while valid != True:
+
+        try:
+            # Try and convert this number to an integer
+            # If it fails, the exception will occur
+            guess = int(guess)
+        except Exception:
+            # Exception was thrown when trying to convert to number,
+            # Report the issue and ask again
+            print('Invalid input; please enter a whole number.')
+            valid = False
+            guess = get_guess()
+
+        # If they get here, it means the number must have been valid
+        # Set valid to be true to break out of the while loop
+        valid = True
+
+    return guess
+
+def compare(numA, numB):
+    if numA > numB:
+        return False
+        print('too high')
+    elif numA < numB:
+        return False
+        print('too low')
+    elif numA == numB:
+        return True
+
+
+def play():
+
+    # Pick a secret number
+    secret_number = random.randint(1, 100)
+
+    # When building your program, the following line will tell you what
+    # the secret_number is x, this will make it easier to test the game.
+    # When done, remove or comment-out this line.
+    print('TEMPORARY DEBUGGING HELPER -> The secret number is: ' + str(secret_number))
+
+    # Print message at the start the game
+    print("\nI'm thinking of a number between 1 and 100; what do you think it is?")
+
+    # Get the player_s initial guess
+    guess = get_guess
+
+    # Keep prompting until they get it correct
+    # For every failed attempt, print 'Too x. Guess again.' where x is either 'high' or 'low'
+    while compare(guess, secret_number) == False:
+        print('Try again')
+        guess = get_guess
+
+    # Print conclusion
+    print('You got it! The number was ' + str(secret_number))
+
+# Run the game
+play()
